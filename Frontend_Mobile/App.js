2c6b624fdec545572907a73291df159be7515946
Faizal Amaan <faizalamaan2@gmail.com>
2024-09-04 00:24:25 +0530

    hello

--- Frontend_Mobile/App.js
+++ Frontend_Mobile/App.js
@@ -1,19 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TouchableNativeFeedback, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  View,
+} from "react-native";
+import ShoppingList from "./app/screens/ShoppingList";
 
 export default function App() {
   const [num, setNum] = useState(0);
   const inc = () => {
     setNum(num + 1);
   };
-  return (
-    <View style={styles.container}>
-      <Text>{num}</Text>
-      <Button title="Hello" onPress={inc} />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <ShoppingList />;
 }
 
 const styles = StyleSheet.create({
---
+++ Frontend_Mobile/app/screens/ShoppingList.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export default class ShoppingList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>ShoppingList</Text>
+        
+      </View>
+    );
+  }
+}
