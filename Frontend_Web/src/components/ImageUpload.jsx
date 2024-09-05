7dcae5925490149ace78f3cebd5e435f83118452
FA Sukry <IT22052124@my.sliit.lk>
2024-09-05 21:13:12 +0530

    update

--- Frontend_Web/src/components/ImageUpload.jsx
+++ Frontend_Web/src/components/ImageUpload.jsx
@@ -2,52 +2,56 @@ import { useState } from "react";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage } from "../storage/firebase";
 
-const ImageUpload = ({ downloadURL, setDownloadURL }) => {
-  const [image, setImage] = useState(null);
+const ImageUpload = ({ DownloadURLs , setDownloadURLs }) => {
+  const [images, setImages] = useState([]);
   const [progress, setProgress] = useState(0);
-  const [isUploaded, setIsUploaded] = useState(false);
 
   const handleImageUpload = (e) => {
-    const file = e.target.files[0];
-    setImage(file);
+    const files = Array.from(e.target.files);
+    setImages(files);
   };
 
-  const uploadImage = () => {
-    if (image) {
+  const uploadImages = () => {
+    const promises = [];
+
+    images.forEach((image) => {
       const storageRef = ref(storage, `images/${image.name}`);
       const uploadTask = uploadBytesResumable(storageRef, image);
 
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const prog = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+      promises.push(
+        new Promise((resolve, reject) => {
+          uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+              const prog = Math.round(
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+              );
+              setProgress(prog);
+            },
+            (error) => {
+              console.log(error);
+              reject(error);
+            },
+            () => {
+              getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+                resolve(url);
+              });
+            }
           );
-          setProgress(prog);
-        },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-            setDownloadURL(url);
-            setIsUploaded(true);
-          });
-        }
+        })
       );
-    }
+    });
+
+    Promise.all(promises).then((urls) => {
+      setDownloadURLs(urls); // Save all the uploaded image URLs
+    });
   };
 
   return (
     <div>
-      {!isUploaded && (
-        <>
-          <input type="file" onChange={handleImageUpload} />
-          <button onClick={uploadImage}>Upload</button>
-        </>
-      )}
+      <input type="file" onChange={handleImageUpload} multiple />
+      <button onClick={uploadImages}>Upload</button>
       <h3>Upload Progress: {progress}%</h3>
-      {downloadURL && <img src={downloadURL} alt="Uploaded" />}
     </div>
   );
 };
